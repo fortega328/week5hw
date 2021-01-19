eb24c15ca50446da280482b93e528e9d9a12378c
fortega328 <fortega328@gmail.com>
2021-01-19 11:17:51 -0500

    Update questions.js
    forced updated

--- assets/questions.js
+++ assets/questions.js
@@ -1,17 +1,19 @@
 // question array
-let questionsArray =[
-    "commonly used data types DO NOT include",
-    "the condition in an if/else statement is enclosed within ____.",
-    "arrays in javascript can be used to store ___.",
-    "string values must be enclosed within ___ when being assigned to variables",
-    "a very useful tool used during development and debugging for printing content to the debugger is:",
-]
-// answers array
-let answersArray = [
-    "strings", 
-    "booleans",
-    "numbers",
-    "quotes",
-    "curly brackets",
-    "all of the above"
-]
+let questionsArray = [{
+    question: "commonly used data types DO NOT include",
+    choicesArray: [
+        "the condition in an if/else statement is enclosed within ____.",
+        "arrays in javascript can be used to store ___.",
+        "string values must be enclosed within ___ when being assigned to variables",
+        "a very useful tool used during development and debugging for printing content to the debugger is:"],
+    answer: "arrays in javascript can be used to store ___."
+},
+{
+    question: "this is another question?",
+    choicesArray: [
+        "choice one",
+        "choice two",
+        "choice three"
+    ],
+    answer: "choice two"
+}]
